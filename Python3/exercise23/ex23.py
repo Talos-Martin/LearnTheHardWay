5e0f0053a8644479a945c5386c3cc85eb4ed7b4a
mzeiser@cisco.com <mzeiser@cisco.com>
2020-04-30 16:51:52 +0200

    23

---
+++ Python3/exercise23/ex23.py
@@ -0,0 +1,45 @@
+import sys
+script, input_encoding, error = sys.argv    #why not just argv?
+
+
+
+#####################################################################################################################################################
+
+
+#function main, reads a single line from file passed as parameter, calls printline to display input encoded and decoded
+#then recursively calls itself with same parameters, file pointer is now at next line though
+#stops when nothing more to read(end of file)
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)    #recursive call
+
+#####################################################################################################################################################
+
+def print_line(line, encoding, errors):
+    next_lang=line.strip()  #removes trailing \r\n? Need to look up. Yep, strips whitespace from end annd also beginning
+
+    #encodes the line it read from the file as told via commandline, from UTF-8(see file open, where thats hardcoded)
+    raw_bytes=next_lang.encode(encoding, errors=errors) #weird errors use. why errors = errors??
+
+    #decodes encoded line as told via commandline
+    cooked_string=raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<========>", cooked_string)
+
+#####################################################################################################################################################
+
+
+#entry point. script starts here
+languages=open("languages.txt", encoding="UTF-8") #opens file as UTF-8. Why is that necessary? Why not just open() without encoding specified? 
+
+#languages is the file handle for the just-opened file languages.txt
+#input-encoding and error and from the commandline
+main(languages, input_encoding, error)
+
+#we have error set to 'strict' on the commandline:
+# "Other possible values are 'ignore', 'replace', 'xmlcharrefreplace', 'backslashreplace' and any other name registered via codecs.register_error()."
+
+#Other possible encoding values:
+# https://docs.python.org/3/library/codecs.html#standard-encodings
